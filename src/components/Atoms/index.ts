46f0d4fe9bdf2d46be500a3436306f23e1e513f5
songforthemute <huni0423@naver.com>
2023-06-29 18:09:57 +0900

    [FIX] Test: Cannot find module './atoms'
    FAIL src/__test__/example.test.tsx
      ● Test suite failed to run
    
        Cannot find module './atoms' from 'src/components/index.ts'
    
        Require stack:
          src/components/index.ts
          src/pages/example.tsx
          src/__test__/example.test.tsx

--- src/components/Atoms/index.ts
+++ src/components/Atoms/index.ts
@@ -1,4 +1,4 @@
-export { default as Button } from "./Button";
-export { default as Input } from "./Input";
-export { default as Select } from "./Select";
-export { default as Modal } from "./Modal";
+export { default as Button } from "./button";
+export { default as Input } from "./input";
+export { default as Select } from "./select";
+export { default as Modal } from "./modal";
