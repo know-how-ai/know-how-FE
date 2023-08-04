953b51fa2854589de8f232b4a9cf9b6b24c1a596
songforthemute <huni0423@naver.com>
2023-08-04 19:01:36 +0900

    [ADD] ContentButton in Root page
    - Need to abstract later

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,60 @@
+import styled from "styled-components";
 import Layout from "../layout/Layout";
 import { Anchor, Button } from "@components/atoms";
+import { media } from "@components/styles/theme";
+
+const UList = styled.ul`
+    display: flex;
+    list-style: none;
+    justify-content: center;
+    gap: 4rem;
+    align-items: center;
+    margin: 4rem auto;
+    width: 100%;
+
+    ${media.mobile} {
+        flex-direction: column;
+        margin: 1rem auto;
+    }
+`;
+
+const Li = styled.li`
+    width: 100%;
+    max-width: 25%;
+    ${media.mobile} {
+        max-width: 75%;
+    }
+`;
+
+const ButtonTitle = styled.span`
+    display: inline-block;
+    text-align: center;
+    font-size: 2rem;
+    color: ${(p) => p.theme.color.textColor};
+    font-weight: 500;
+    line-height: 3rem;
+`;
 
 export default function Home() {
     return (
         <Layout title="Home">
-            {/* 조건부: 로그인 안되어있을때만. */}
+            <UList>
+                <Li>
+                    <Button size="infinite" shape="square" color="transparent">
+                        <ButtonTitle>자소서 첨삭 봇</ButtonTitle>
+                    </Button>
+                </Li>
+                <Li>
+                    <Button size="infinite" shape="square" color="transparent">
+                        <ButtonTitle>면접 도우미 봇</ButtonTitle>
+                    </Button>
+                </Li>
+                <Li>
+                    <Button size="infinite" shape="square" color="transparent">
+                        <ButtonTitle>직업 추천 봇</ButtonTitle>
+                    </Button>
+                </Li>
+            </UList>
 
             <Anchor href={"/example"}>
                 <Button>Go to Example</Button>
