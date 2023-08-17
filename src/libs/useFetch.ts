23f3da015ac2051e88cb6528601fa6188cc2d2bd
songforthemute <huni0423@naver.com>
2023-08-17 22:11:12 +0900

    [ADD] useFetch Hook
    - Need to writing test

---
+++ src/libs/useFetch.ts
@@ -0,0 +1,23 @@
+type Method = "GET" | "POST" | "PUT" | "DELETE";
+type useFetchType = <T, R>(
+    url: URL | RequestInfo,
+    method: Method,
+    data?: T
+) => Promise<R>;
+
+const useFetch: useFetchType = async (url, method, data) => {
+    const body = data ? JSON.stringify(data) : undefined;
+    const response = await fetch(url, {
+        method,
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body,
+    });
+
+    const json = await response.json();
+
+    return json;
+};
+
+export default useFetch;
