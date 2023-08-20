e84516bd31f906b2cb168d93479c5c8ebe2ec73e
songforthemute <huni0423@naver.com>
2023-08-21 02:06:48 +0900

    [ADD] Set Proxy for backend

--- next.config.js
+++ next.config.js
@@ -5,6 +5,15 @@ const nextConfig = {
     compiler: {
         styledComponents: true,
     },
+    // 쿠키 발행 테스팅을 위한 프록시 설정
+    async rewrites() {
+        return [
+            {
+                source: "/:path*",
+                destination: "http://localhost:8080/:path*",
+            },
+        ];
+    },
 };
 
 module.exports = nextConfig;
