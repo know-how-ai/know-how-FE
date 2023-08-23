937311b8624fe763b5711abb5c2783a7e933585d
songforthemute <huni0423@naver.com>
2023-08-23 20:24:32 +0900

    [ADD] Delete page & feature
    - add `/delete` page & feature.

--- src/components/organics/profileModal/ProfileModal.tsx
+++ src/components/organics/profileModal/ProfileModal.tsx
@@ -1,7 +1,7 @@
-import { Button, Modal } from "@components/atoms";
+import { Anchor, Button, Modal } from "@components/atoms";
 import { LogTable } from "@components/molecules";
 import { useAppDispatch } from "@contexts/contextHooks";
-import { offModal, setToast } from "@contexts/uiSlice";
+import { setToast } from "@contexts/uiSlice";
 import { useUserSelector } from "@contexts/userSlice";
 import useFetch from "@libs/useFetch";
 import { useCallback, useEffect, useState } from "react";
@@ -57,6 +57,8 @@ interface IProfileModal {
 const greeting = <span>님 반가워요!</span>;
 const restPoint = <span>잔여 포인트 : </span>;
 
+const LOGS_URL = `/user/log`;
+
 const ProfileModal = ({
     handleClose,
     point,
@@ -69,28 +71,27 @@ const ProfileModal = ({
     const [logs, setLogs] = useState<ILog[]>([]);
     const [index, setIndex] = useState<number>(0);
 
-    const useGetLogs = useCallback(() => {
-        useFetch<any, ILogResponse>(
-            `/user/log?skip=${logs.length}`,
+    const useGetLogs = useCallback(async () => {
+        const { data, error } = await useFetch<any, ILogResponse>(
+            `${LOGS_URL}?skip=${logs.length}`,
             "GET"
-        ).then((response) => {
-            if (response.data) {
-                setLogs([...logs, ...(response?.data?.logs || [])]);
-                setIndex(index + 1);
-            } else if (response.error) {
-                dispatch(setToast({ toast: response.error }));
-            }
-        });
+        );
+
+        if (data) {
+            setLogs([...logs, ...(data?.logs || [])]);
+            setIndex(index + 1);
+        } else if (error) {
+            dispatch(setToast({ toast: error }));
+        }
     }, [index, logs.length]);
 
     useEffect(() => {
-        if (!isLoggedIn) {
-            dispatch(setToast({ toast: "로그인이 필요합니다." }));
-            dispatch(offModal());
-        } else {
+        if (isLoggedIn) {
             useGetLogs();
+        } else {
+            dispatch(setToast({ toast: "로그인이 필요합니다." }));
         }
-    }, []);
+    }, [isLoggedIn]);
 
     return (
         <Modal handleClose={handleClose}>
@@ -159,9 +160,12 @@ const ProfileModal = ({
                 </nav>
 
                 <Button onClick={handleLogout}>로그아웃</Button>
-                <button onClick={() => {}}>
-                    <Span>서비스를 그만 이용하고 싶어요.</Span>
-                </button>
+
+                <Anchor href={"/delete"}>
+                    <button>
+                        <Span>서비스를 그만 이용하고 싶어요.</Span>
+                    </button>
+                </Anchor>
             </Container>
         </Modal>
     );
---
+++ src/pages/delete.tsx
@@ -0,0 +1,113 @@
+import { LabelWrapper } from "@components/molecules";
+import Layout from "../layout/Layout";
+import { Button, Form, Heading, Input } from "@components/atoms";
+import type { NextPage } from "next";
+import { useForm } from "react-hook-form";
+import { useRouter } from "next/router";
+import styled from "styled-components";
+import useFetch from "@libs/useFetch";
+import { useAppDispatch } from "@contexts/contextHooks";
+import { setToast } from "@contexts/uiSlice";
+import { loggedOut } from "@contexts/userSlice";
+
+interface IDeleteForm {
+    email: string;
+    password: string;
+}
+
+const Paragraph = styled.p`
+    margin: auto;
+    line-height: 2rem;
+    font-size: 1.2rem;
+    color: ${(p) => p.theme.color.gray};
+`;
+
+interface ResponseReturn {
+    status: boolean;
+    error?: string;
+}
+
+const title = "서비스 탈퇴";
+
+const DELETE_URL = `/user/delete`;
+
+const Delete: NextPage = () => {
+    const dispatch = useAppDispatch();
+
+    const { replace } = useRouter();
+    const { register, handleSubmit, setError } = useForm<IDeleteForm>({
+        mode: "onBlur",
+    });
+
+    const onSubmit = async (formData: IDeleteForm) => {
+        try {
+            const { status, error } = await useFetch<
+                IDeleteForm,
+                ResponseReturn
+            >(DELETE_URL, "POST", formData);
+
+            if (status && !error) {
+                replace("/");
+                dispatch(loggedOut());
+                dispatch(
+                    setToast({
+                        toast: "이용해주셔서 감사합니다.",
+                    })
+                );
+            } else if (error) {
+                dispatch(setToast({ toast: error }));
+            }
+        } catch (err) {
+            console.error(err);
+        }
+    };
+
+    return (
+        <Layout title={title} widgets={{ theme: true, profile: true }}>
+            <Heading>{title}</Heading>
+
+            <section style={{ margin: "1rem auto" }}>
+                <Paragraph>탈퇴 후, 데이터는 복구할 수 없습니다.</Paragraph>
+                <Paragraph>
+                    정말 탈퇴하시려면 이메일과 패스워드를 입력해주세요.
+                </Paragraph>
+            </section>
+
+            <Form
+                onSubmit={handleSubmit(onSubmit)}
+                display="flex"
+                flexDirection="column"
+                justifyContent="center"
+                gap={2}
+            >
+                <LabelWrapper label="이메일">
+                    <Input
+                        required
+                        type="email"
+                        placeholder="id@example.com"
+                        register={register("email", {
+                            required: true,
+                        })}
+                    />
+                </LabelWrapper>
+
+                <LabelWrapper label="패스워드">
+                    <Input
+                        required
+                        type="password"
+                        placeholder="****"
+                        register={register("password", {
+                            required: true,
+                        })}
+                    />
+                </LabelWrapper>
+
+                <Button type="submit">탈퇴하기</Button>
+            </Form>
+        </Layout>
+    );
+};
+
+Delete.displayName = "Interview";
+
+export default Delete;
