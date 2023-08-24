ceffe383a81e67cc0aca97b2cf8f075555c9d902
songforthemute <huni0423@naver.com>
2023-08-24 19:56:30 +0900

    [ADD] `ResultContainer` Organic
    - need to write the storybook, unit test

--- src/components/organics/index.ts
+++ src/components/organics/index.ts
@@ -1,3 +1,4 @@
 export { default as ProfileModal } from "./profileModal";
 export { default as AuthModal } from "./authModal";
 export { default as HeaderWidgets } from "./headerWidgets";
+export { default as ResultContainer } from "./resultContainer";
---
+++ src/components/organics/resultContainer/ResultContainer.tsx
@@ -0,0 +1,54 @@
+import { Button } from "@components/atoms";
+import { media } from "@components/styles/theme";
+import { useRouter } from "next/router";
+import { ReactNode, type FC } from "react";
+import styled from "styled-components";
+
+const ResultSection = styled.section`
+    display: grid;
+    width: 100%;
+    max-width: 60vw;
+
+    ${media.tablet} {
+        max-width: 75vw;
+    }
+
+    ${media.mobile} {
+        max-width: 90vw;
+    }
+
+    margin: 4rem auto;
+    padding: 1rem;
+    gap: 2rem;
+`;
+
+interface ResultContainerProps {
+    children?: ReactNode | string | any;
+}
+
+const ResultContainer: FC<ResultContainerProps> = ({ children }) => {
+    const { back } = useRouter();
+
+    return (
+        <>
+            <ResultSection>{children}</ResultSection>
+
+            <Button type="button" onClick={() => back()}>
+                다시하기
+            </Button>
+
+            <Button
+                type="button"
+                color="transparent"
+                onClick={() => {
+                    back();
+                    back();
+                }}
+            >
+                메인으로
+            </Button>
+        </>
+    );
+};
+
+export default ResultContainer;
