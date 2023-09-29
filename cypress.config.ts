70c3c8d08b95436f1dbf457b19d9cd14df88df49
songforthemute <huni0423@naver.com>
2023-09-30 00:35:48 +0900

    [ADD] install cypress & setup

---
+++ cypress.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from "cypress";
+
+module.exports = defineConfig({
+    e2e: {
+        baseUrl: "http://localhost:3000",
+        setupNodeEvents(on, config) {
+            // implement node event listeners here
+        },
+    },
+    projectId: "ownProjectId",
+});
